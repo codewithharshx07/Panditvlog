9c7dfcc72955d650bfa93ebffc6db9164041788a
𝐇𝐚𝐫𝐬𝐡 𝐑𝐚𝐣 <85678943+codewithharshx07@users.noreply.github.com>
2024-11-11 00:03:47 +0530

    Add files via upload

---
+++ gsap.js
@@ -0,0 +1,40 @@
+
+
+//                       GSAP PROJECT :-
+
+gsap.from("#nav h3",{
+    y:-50,
+
+    duration:1,
+    delay:0.4,
+    stagger:0.2 
+
+
+})
+
+gsap.from("#main h1",{
+    x:-500,
+    opacity:0,
+
+    
+    duration:1,
+    delay:0.8,
+    stagger:0.2,
+
+
+})
+
+gsap.from("img",{
+    x: 100,
+    rotate:45,
+   
+    duration:1.1,
+    opacity:0,
+    stagger:1,
+    
+
+
+})
+
+
+
